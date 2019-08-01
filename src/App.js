a221e56467e14714a257bebf5f00137b5e387fdc
Brandon Ha <brandon.vanha@gmail.com>
2019-08-01 17:47:57 -0400

    Refactor person condition

--- src/App.js
+++ src/App.js
@@ -52,6 +52,26 @@ class App extends Component {
       cursor: 'pointer',
     };
 
+    let persons = null;
+    
+    if (this.state.showPersons) {
+      persons = ( 
+        <div>
+        <Person
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Maze!')} 
+          changed={this.nameChangedHandler} >My hobby: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+      </div>    
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -63,22 +83,7 @@ class App extends Component {
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Switch Name Two</button>
-        { 
-          this.state.showPersons === true ? 
-            <div>
-              <Person
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age} />
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Maze!')} 
-                changed={this.nameChangedHandler} >My hobby: Racing</Person>
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age} />
-            </div> : null
-        }
+        {persons}
       </div>
     );
   }
