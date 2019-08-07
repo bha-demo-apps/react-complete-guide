2470564e9273c6749cd7886f70fbd3faab6a3d0f
Brandon Ha <brandon.vanha@gmail.com>
2019-08-07 17:31:25 -0400

    Restructure folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 // import './App.css';
 import styles from './App.module.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person';
 
 class App extends Component {
 
