be1580ea40461933f5ffa0425b435aa480f1d583
Brandon Ha <brandon.vanha@gmail.com>
2019-08-13 17:06:19 -0400

    Backup

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './Person.css';
+import styles from './Person.module.css';
 import Aux from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
   render() {
@@ -19,4 +20,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, styles.Person);
